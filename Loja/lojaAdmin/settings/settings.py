444f8c381b83703e5685b3e85f574751fbd092dd
gasilvabr <gasilvabr@hotmail>
2026-04-13 10:19:36 -0300

    Fim do capitulo 3

--- Loja/lojaAdmin/settings/settings.py
+++ Loja/lojaAdmin/settings/settings.py
@@ -10,12 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from django.conf.urls.static import static
+from django.conf import settings
 from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
