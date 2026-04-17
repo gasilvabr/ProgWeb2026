2cdb9b7d05d7f4cef85e070cc456fe3bccd5c05f
gasilvabr <gasilvabr@hotmail>
2026-04-17 11:23:51 -0300

    Alteracoes no admin

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import * #importa nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+
+
+admin.site.register(Fabricante,FabricanteAdmin) #adiciona a interface do adm
 admin.site.register(Categoria)
 admin.site.register(Produto)
