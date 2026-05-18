b7211eb337912cbb2cb3c85ff8a159ed2264eda4
gasilvabr <gasilvabr@hotmail>
2026-05-18 09:12:37 -0300

    Cap 6

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,5 +1,43 @@
 from django.http import HttpResponse
+from loja.models import Produto
+
 def list_produto_view(request, id=None):
+    # Carrega informações da requisição
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+    # Carregar informações do banco de dados
+    #produtos = Produto.objects.all()
+    #produtos = Produto.objects.first()
+    #produtos = Produto.objects.filter(Produto=produto)
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    # mostrar no console
+    print(produtos)
+    # Verifica informações enviadas na requisição
+    if destaque is not None:
+        print(destaque)
+    if produto is not None:
+        print(produto)
+    if promocao is not None:
+        print(promocao)
+    if categoria is not None:
+        print(categoria)
+    if fabricante is not None:
+        print(fabricante)
     #if id is None:
     #    id = 0
     if id is None:
--- Loja2/loja/views/ProdutoView.py
+++ Loja2/loja/views/ProdutoView.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
+from loja.models import Produto
+
 def list_produto_view(request, id=None):
+    # Carrega dados do navegador
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+    # carrego dados do baco de dados
+    #produtos = Produto.objects.all()
+    #produtos = Produto.objects.first()
+    produtos = Produto.objects.filter(Produto=produto)
+    print(produtos)
+    if destaque is not None:
+        print(destaque)
+    if produto is not None:
+        print(produto)
+    if promocao is not None:
+        print(promocao)
+    if categoria is not None:
+        print(categoria)
+    if fabricante is not None:
+        print(fabricante)
+    
     #if id is None:
     #    id = 0
     if id is None:
