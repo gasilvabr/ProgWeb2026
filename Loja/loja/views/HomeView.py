ba8db6093ffe73e5190155be51b0b9e7202238ee
gasilvabr <gasilvabr@hotmail>
2026-05-29 09:33:03 -0300

    cap 7

--- Loja/loja/views/HomeView.py
+++ Loja/loja/views/HomeView.py
@@ -1,5 +1,15 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
+from loja.models import Produto
+
 def home_view(request):
-    #return HttpResponse('<h1>Olá mundo!</h1>')
-    return render(request, template_name='home/home.html', status=200)
+    # Le nome do produto digitao pelo usuario e consulta banco de dados
+    produto = request.GET.get("produto")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)    
+    # Monto o contexto comos dados necessarios a visualização da pagina        
+    context = {
+        'produtos': produtos
+    }
+    return render(request, template_name='home/home.html', context=context, status=200)
--- Loja2/loja/views/HomeView.py
+++ Loja2/loja/views/HomeView.py
@@ -1,5 +1,17 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
+from loja.models import Produto
+
 def home_view(request):
-    #return HttpResponse('<h1>Olá mundo!</h1>')
-    return render(request, template_name='home/home.html', status=200)
+    # Filtra o produto no banco de dados, criando uma lista com eles
+    produto = request.GET.get("produto")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    # Cria uma variavel de contexto para passar para o template com a lista
+    context = {
+        'produtos': produtos
+    }
+    print(produtos)
+
+    return render(request, template_name='home/home.html', context=context, status=200)
