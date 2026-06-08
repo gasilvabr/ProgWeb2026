f8ac1fde186af55ed8fb54be9737cdc343e1df8e
gasilvabr <gasilvabr@hotmail>
2026-06-08 09:14:46 -0300

    cap 7

--- Loja2/loja/views/ProdutoView.py
+++ Loja2/loja/views/ProdutoView.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render # from django.http import HttpResponse
+from django.shortcuts import render, redirect # from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
@@ -66,3 +66,42 @@ def edit_produto_view(request, id=None):
     print(produto)
     context = { 'produto': produto }
     return render(request, template_name='produto/produto-edit.html', context=context, status=200)
+
+# adicione a função que trata o postback da interface de edição
+def edit_produto_postback(request, id=None):
+    # Processa o post back gerado pela action    
+    if request.method == 'POST':
+        # Salva dados editados
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+            obj_produto.save()
+            print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
+
+def details_produto_view(request, id=None):
+    # Processa o evento GET gerado pela action
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-details.html', context=context, status=200)
