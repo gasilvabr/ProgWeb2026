e6874a55634ae4aa25142e062e2a758014c5e3ae
gasilvabr <gasilvabr@hotmail>
2026-06-26 09:18:13 -0300

    cap8

--- Loja2/loja/admin.py
+++ Loja2/loja/admin.py
@@ -18,3 +18,5 @@ class ProdutoAdmin(admin.ModelAdmin):
 admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do adm
 admin.site.register(Categoria)
 admin.site.register(Produto, ProdutoAdmin)
+# incluir a tabela de usuário no final
+admin.site.register(Usuario)
